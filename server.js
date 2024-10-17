87acbebdaa56778bf8b984f0722685db3bf39d3e
Juan <juancomes312@gmail.com>
2024-10-16 22:07:10 -0300

    localhost

--- server.js
+++ server.js
@@ -1,27 +1,38 @@
 import express from 'express';
 import router from './routes/index.js';
 import mongoose from 'mongoose';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+// Todo esto para que funcione lo de Pledu
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-
-const url = "mongodb+srv://juancomes54:6vojNqZUvqiSIZeg@proyecto.4lap9.mongodb.net/?retryWrites=true&w=majority&appName=Proyecto"
+const url = "mongodb+srv://juancomes54:6vojNqZUvqiSIZeg@proyecto.4lap9.mongodb.net/?retryWrites=true&w=majority&appName=Proyecto";
 const app = express();
 const PORT = 3000;
-
-
 
 // Middleware para usar el router
 app.use('/', router);
 app.use(express.json());
 
-const connectToMongo = ()=>{
-  mongoose.conecct(url)
-} 
+// Función para conectar a MongoDB
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(url);
+    console.log("Conexión a MongoDB exitosa");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Ejecutar la función para conectar a MongoDB
+connectToMongo();
 
 // Levantar el servidor
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en puerto 3000`);
+  console.log(`Servidor escuchando en puerto ${PORT}`);
 });
 
-
-
+// Configuración de archivos estáticos
+app.use(express.static(path.join(__dirname, "public")));
